cc193a3bf6dd88613873635e40d8e7839b864e8e
StavrosJKw <stavroskotsilas@gmail.com>
2024-10-22 09:53:17 +0300

    distance tests passed

--- DistanceAcutest/readVectors.cpp
+++ DistanceAcutest/readVectors.cpp
@@ -71,11 +71,7 @@ vector<DataVector<float>> ReadVectorFile(const string& filename) {
     return dataVectors; // Return the array of DataVector objects
 }
 
-<<<<<<< HEAD
 /*int main(void) {
-=======
-int main(void) {
->>>>>>> 0ef4440 (trying to figure out distance test)
     //read the base vectors
     vector<DataVector<float>> baseDataVector = ReadVectorFile("../data/siftsmall/siftsmall_base.fvecs");
     //save the base vectors
@@ -88,24 +84,13 @@ int main(void) {
 
 
     // Access the first vector's data as an example
-<<<<<<< HEAD
-    if (!baseDataVector.empty()) {
-=======
     /*if (!baseDataVector.empty()) {
->>>>>>> 0ef4440 (trying to figure out distance test)
         DataVector<float>& firstVector = baseDataVector[0]; // From the first vector
         cout << "First vector, first value: " << firstVector.getDataAtIndex(0) << endl;
         cout << "First vector, second value: " << firstVector.getDataAtIndex(1) << endl;
         cout << "First vector, third value: " << firstVector.getDataAtIndex(2) << endl;
-<<<<<<< HEAD
-    }
-
-    return 0;
-}*/
-=======
     }*/
 
     return 0;
 }
->>>>>>> 0ef4440 (trying to figure out distance test)
 
