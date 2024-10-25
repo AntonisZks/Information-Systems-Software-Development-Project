ceffc0b6af052dfbd453ba799eef4eb5bcb6b188
EvitaChasioti <sdi2100289@di.uoa.gr>
2024-10-25 12:35:16 +0300

    GreedySearch

--- include/vanama.h
+++ include/vanama.h
@@ -5,7 +5,12 @@
 // this contains most of the STL headers files (data structures) amd iostream
 // WARNING individual #inlcludes to ensure portability
 // temporary placeholder for clean code
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <set>
 
 // DECLARATIONS
 
--- src/graph_functions.cpp
+++ src/graph_functions.cpp
@@ -8,12 +8,20 @@
 // this contains most of the STL headers files (data structures) amd iostream
 // WARNING individual #inlcludes to ensure portability
 // temporary placeholder for clean code
-#include <bits/stdc++.h>
+
 
 // Custom header files
 #include "graphFunctions.h"
 #include "distance.h"
 #include "vanama.h"
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <queue>
+#include <set>
+#include <cmath>
+#include <functional>
 
 //######################################################################################
 
@@ -28,7 +36,54 @@
  * @param k the result size
  * @param L the search list size
 */
-template<typename graph_t> void GreedySearch(Graph<graph_t> graph, graph_t s, DataVector<graph_t> query, int k, int L){
+
+struct NodeData {
+    double x;  
+    double y;  
+
+     
+    double distanceTo(const NodeData &other) const {
+        return sqrt((x - other.x) * (x - other.x) + (y - other.y) * (y - other.y));
+    }
+};
+
+/*struct Pair {
+    int first;
+    int second;
+};
+using pair = std::pair<int, int>*/
+
+template <typename T>
+void GreedySearch(Graph<T> &graph, NodeData query, int k) {
+    using Pair = std::pair<double, T>;
+    std::priority_queue<Pair, std::vector<Pair>, std::greater<Pair> > search_queue;
+    std::set<T> visited;
+    using Pair = std::pair<double, T>;
+
+     
+    for (const auto& node : graph.nodes) {
+        double dist = node.second.distanceTo(query);
+        search_queue.push(Pair(dist, node.first));
+    }
+
+    std::vector<Pair> result;
+
+    
+    while (!search_queue.empty() && result.size() < k) {
+        Pair closest = search_queue.top();
+        search_queue.pop();
+        
+         
+        if (visited.count(closest.second) == 0) {
+            visited.insert(closest.second);
+            result.push_back(closest);
+        }
+    }
+
+    std::sort(result.begin(), result.end(), [](const Pair &a, const Pair &b) {
+        return a.first < b.first; 
+    });
+
 
 }
 
