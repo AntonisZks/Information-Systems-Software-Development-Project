c331f144227c9df86e1806c0c861363ffba205b7
EvitaChasioti <100272639+EvitaChasioti@users.noreply.github.com>
2024-11-04 22:26:02 +0200

    Comments

--- include/Evaluation/recall.h
+++ include/Evaluation/recall.h
@@ -3,15 +3,30 @@
 #include <algorithm>
 #include "../DataStructures/DataVector/DataVector.h"
 
-
+/**
+ * @brief Calculates the recall evaluation metric.
+ *
+ * This function computes the recall, which is the fraction of relevant instances 
+ * that have been retrieved over the total amount of relevant instances. It is 
+ * defined as the size of the intersection of sets X and G divided by the size of set G.
+ *
+ * @tparam graph_t The type of the elements in the sets.
+ * @param X A set of retrieved instances.
+ * @param G A set of relevant instances.
+ * @return The recall value as a double.
+ */
 template <typename graph_t>
 double calculateRecallEvaluation(const std::set<graph_t>& X, const std::set<graph_t>& G) {
 
-  std::set<graph_t> result;
-  std::set_intersection(X.begin(), X.end(), G.begin(), G.end(), std::inserter(result, result.begin()));
+// Declare a set to store the intersection result
+std::set<graph_t> result;
 
-  unsigned int k = G.size();
+// Compute the intersection of sets X and G, and insert the result into 'result'
+std::set_intersection(X.begin(), X.end(), G.begin(), G.end(), std::inserter(result, result.begin()));
 
-  return (double)result.size() / (double)k;
+// Get the size of set G
+unsigned int k = G.size();
 
+// Return the ratio of the size of the intersection to the size of G
+return (double)result.size() / (double)k;
 }
