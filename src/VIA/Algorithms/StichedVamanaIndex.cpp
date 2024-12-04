7c0df9aac0e5c181f1e99c14cf9d73b330fc98ff
Antonis Zikas <sdi2100038@di.uoa.gr>
2024-12-04 13:36:16 +0000

    Started the Filtered Vamana call

--- src/VIA/Algorithms/StichedVamanaIndex.cpp
+++ src/VIA/Algorithms/StichedVamanaIndex.cpp
@@ -40,23 +40,29 @@ void StichedVamanaIndex<vamana_t>::createGraph(
 
   // Foreach f in F do
   for (auto filter : this->F) {
-    std::cout << "Creating graph for filter: " << filter.getC() << std::endl;
+    
     std::vector<vamana_t> currentVector = Pf[filter];
     
-    // Keep the indexes of the current filter points in P
+    // Keep the indexes of the current filter points in P, to connect them later. The idea here is to create a new
+    // sub-index of type VamanaIndex that will contain the points of the current filter, which are going to have a different
+    // index inside the new sub-graph. So we need to keep track of the indexes of the points in the original graph.
     std::map<unsigned int, unsigned int> indexes;
     for (unsigned int i = 0; i < currentVector.size(); i++) {
       vamana_t currentData = currentVector[i];
       indexes[i] = currentData.getIndex();
     }
 
+    // Initialize the sub-index for the current filter and create its graph
     VamanaIndex<vamana_t> subIndex;
     subIndex.createGraph(Pf[filter], alpha, R_small, L_small, false);
 
     for (unsigned int i = 0; i < subIndex.getGraph().getNodesCount(); i++) {
+      
+      // Get the current node from the sub-index and its index in the sub-graph
       GraphNode<vamana_t>* node = subIndex.getGraph().getNode(i);
       unsigned int nodeIndex = node->getData().getIndex();
 
+      // Receive all the neighbors of the current node and connect them in the main graph, using the indexes map above
       std::vector<vamana_t>* neighbors = node->getNeighborsVector();
       for (auto neighbor : *neighbors) {
         unsigned int currentNeighborIndex = neighbor.getIndex();
@@ -67,10 +73,20 @@ void StichedVamanaIndex<vamana_t>::createGraph(
 
   }
 
+  // Filtered Robust Prune for every v in V (the nodes of the graph)
   for (unsigned int i = 0; i < this->G.getNodesCount(); i++) {
-    std::cout << "Node " << i << " with filter " << this->G.getNode(i)->getData().getC() << " has number of neighbors: ";
-    std::cout << this->G.getNode(i)->getNeighborsVector()->size() << std::endl;
+
+    GraphNode<vamana_t>* currentNode = this->G.getNode(i);
+    std::vector<vamana_t>* neighbors = currentNode->getNeighborsVector();
+
+    // TODO: Implement Filtered Vamana call
+
   }
+
+  // for (unsigned int i = 0; i < this->G.getNodesCount(); i++) {
+  //   std::cout << "Node " << i << " with filter " << this->G.getNode(i)->getData().getC() << " has number of neighbors: ";
+  //   std::cout << this->G.getNode(i)->getNeighborsVector()->size() << std::endl;
+  // }
 
 }
 
